c8139f3555bacef1de3eea042a48b6832b9e610f
mahima145 <maahibhatnagar@gmail.com>
2018-08-15 12:28:14 +0530

    Strings.cc

---
+++ String.cc
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+#include <string.h>
+using namespace std;
+//reverse input string
+void reverse(char *s,int len)
+{
+     char *s_new= new char[len];
+     int j=len-1;
+     cout<<"original str: "<<s<<endl;
+  for(int i=0;i<len;i++)
+  {
+      
+          s_new[i] = s[i];
+      j--;
+      
+  }
+  cout<<"reverse str: "<<s_new<<endl;
+  delete[] s_new;
+}
+//convert input str into str without vowel
+void str_witout_vowel(char *s,int len)
+{
+    char *s_new= new char[len];
+     int j=0;
+     cout<<"original str: "<<s<<endl;
+  for(int i=0;i<len;i++)
+  {
+      if(!(s[i]=='a' || s[i]=='i'||s[i]=='e'||s[i]=='o'||s[i]=='u'))
+      {
+          s_new[j++] = s[i];
+      }
+     
+  }
+   s_new[j++]='\0';
+   cout<<"str w/o vowel: "<<s_new<<endl;
+   delete [] s_new;
+}
+//check if a string is Palindrome
+void isPalindrome(char *s,int len)
+{
+    bool isPalindrome = true;
+    for(int i=0;i<=len/2;i++)
+  {
+      if(s[i] !=s[len--])
+      {
+         isPalindrome = false;
+         break;
+      }
+     
+  }
+  if(isPalindrome)
+  cout<<"str is Palindrome "<<endl;
+  else
+  cout<<"str is not a Palindrome "<<endl;
+}
+int main()
+{
+     char s[]="hello mahima";
+     int len =strlen(s);
+  reverse(s,len);
+  str_witout_vowel(s,len);
+  isPalindrome("maiham",6);
+    return 0;
+}
+
